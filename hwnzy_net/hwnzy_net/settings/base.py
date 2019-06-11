77454aa7eac13e85083d6a85bd9c8445f53ef4b1
hwnzy <hwnzy@foxmail.com>
2019-06-11 17:56:39 +0800

    项目的一些初始化设置

--- hwnzy_net/hwnzy_net/settings/base.py
+++ hwnzy_net/hwnzy_net/settings/base.py
@@ -25,10 +25,6 @@ STR_IP = '127.0.0.1'
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'ev8#_ik6edui6m5t3i=7zb!r(52fn(6wj1#bv5r38a4&=&5_*^'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-
 
 ALLOWED_HOSTS = []
 
@@ -149,46 +145,3 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # 配置静态文件加载路径
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
-
-# 配置工程的日志
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,  # 是否禁用已经存在的日志器
-    'formatters': {  # 日志信息显示的格式
-        'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s %(lineno)d %(message)s'
-        },
-        'simple': {
-            'format': '%(levelname)s %(module)s %(lineno)d %(message)s'
-        },
-    },
-    'filters': {  # 对日志进行过滤
-        'require_debug_true': {  # django在debug模式下才输出日志
-            '()': 'django.utils.log.RequireDebugTrue',
-        },
-    },
-    'handlers': {  # 日志处理方法
-        'console': {  # 向终端中输出日志
-            'level': 'INFO',
-            'filters': ['require_debug_true'],
-            'class': 'logging.StreamHandler',
-            'formatter': 'simple'
-        },
-        'file': {  # 向文件中输出日志
-            'level': 'INFO',
-            'class': 'logging.handlers.RotatingFileHandler',
-            'filename': os.path.join(os.path.dirname(BASE_DIR), 'logs/hwnzy_net.log'),  # 日志文件的位置
-            'maxBytes': 300 * 1024 * 1024,
-            'backupCount': 10,
-            'formatter': 'verbose'
-        },
-    },
-    'loggers': {  # 日志器
-        'django': {  # 定义了一个名为django的日志器
-            'handlers': ['console', 'file'],  # 可以同时向终端与文件中输出日志
-            'propagate': True,  # 是否继续传递日志信息
-            'level': 'INFO',  # 日志器接收的最低日志级别
-        },
-    }
-}
